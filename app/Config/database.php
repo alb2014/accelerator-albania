4da3d32416a2a84e3ddba59f0cf4be5e9739d5b4
Phill M Wilt <phill@phillwilt.com>
2013-11-27 18:15:09 -0800

    changed up db config.... ladiez

--- app/Config/database.php
+++ app/Config/database.php
@@ -76,29 +76,33 @@
  *
  */
 class DATABASE_CONFIG {
-
-	public $default;
-  function __construct() {
-    $this->default = array(
-      'datasource' => 'Database/Mysql',
-      'persistent' => false,
-      'host'       => str_replace("'", '', getenv('AA_DB_HOST')),
-      'login'      => str_replace("'", '', getenv('AA_DB_USER')),
-      'password'   => str_replace("'", '', getenv('AA_DB_PASS')),
-      'database'   => str_replace("'", '', getenv('AA_DB_NAME')),
-      'prefix'     => '',
-      'encoding'   => 'utf8',
-    );
-  }
-
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
-		'encoding' => 'UTF8',
-	);
+ 
+    function __construct() {
+ 
+        $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+ 
+        $this->default = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => $url['host'],
+            'login' => $url['user'],
+            'password' => $url['pass'],
+            'database' => substr($url['path'],1),
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+ 
+        $this->test = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'user',
+            'password' => 'password',
+            'database' => 'test_database_name',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+ 
+    }
+ 
 }
