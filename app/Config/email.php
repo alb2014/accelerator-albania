c2e44e6e6bcd3381228ccaf8b50d1ad247aa2203
Phill M Wilt <phill@phillwilt.com>
2013-12-01 14:52:24 -0800

    modded email config

--- app/Config/email.php
+++ app/Config/email.php
@@ -6,5 +6,20 @@ class EmailConfig {
         'host' => 'smtp.sendgrid.net',    
         'port' => 587,    
         'username' => 'app19805266@heroku.com',    
-        'password' => 'w8or1w97');    
+        'password' => 'w8or1w97',
+        'log' => true);
+
+        public $smtp = array(
+		'transport' => 'Smtp',
+		'from' => array('app19805266@heroku.com' => 'Albania Accel'),    
+        'host' => 'smtp.sendgrid.net',    
+        'port' => 587,    
+        'username' => 'app19805266@heroku.com',    
+        'password' => 'w8or1w97',
+		'timeout' => 30,
+		'client' => null,
+		'log' => true
+		//'charset' => 'utf-8',
+		//'headerCharset' => 'utf-8',
+	);    
 }?>
