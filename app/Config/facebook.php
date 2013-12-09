4942f2a52512fcceb52c4244bb19eae530744c84
Phill M Wilt <phill@phillwilt.com>
2013-12-09 10:39:47 -0800

    added facebook plugin

---
+++ app/Config/facebook.php
@@ -0,0 +1,11 @@
+<?php //app/Config/facebook.php
+	$config = array(
+		'Facebook' => array(
+			'appId'  => '239655156201853',
+			'apiKey' => 'YOUR_API_KEY',
+			'secret' => '042b44a1834cdb2e9ae58433e1c52edc',
+			'cookie' => true,
+			'locale' => 'en_US',
+		)
+	);
+?>
