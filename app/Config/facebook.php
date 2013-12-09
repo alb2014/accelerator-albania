e1bbbbd5a7df406242a4bf3ee535b3d16ccdb163
Phill M Wilt <phill@phillwilt.com>
2013-12-09 10:48:50 -0800

    added more fb features

--- app/Config/facebook.php
+++ app/Config/facebook.php
@@ -2,7 +2,7 @@
 	$config = array(
 		'Facebook' => array(
 			'appId'  => '239655156201853',
-			'apiKey' => 'YOUR_API_KEY',
+			'apiKey' => '239655156201853',
 			'secret' => '042b44a1834cdb2e9ae58433e1c52edc',
 			'cookie' => true,
 			'locale' => 'en_US',
