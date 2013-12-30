975306e5c3e57a25abe452baa9aa16a8529f44e0
Phill M Wilt <phill@phillwilt.com>
2013-12-30 03:36:17 -0800

    email update

--- app/Config/email.php
+++ app/Config/email.php
@@ -1,27 +1,23 @@
 <?php 
 
-class EmailConfig { 
+class EmailConfig {
 
     public $default = array(    
         'transport' => 'Smtp',    
-        'from' => array(getenv("SENDGRID_USERNAME") => 'Albania Accel'),    
+        'from' => array('app20466457@heroku.com' => 'Albania Accel'),    
         'host' => 'smtp.sendgrid.net',    
         'port' => 587,    
-        'username' => 'app19805266@heroku.com',    
-        'password' => 'w8or1w97',
+        'username' => 'app20466457@heroku.com',    
+        'password' => 'yokrz3fb',
         'log' => true);
 
-        public $smtp = array(
-		'transport' => 'Smtp',
-		'from' => array(getenv("SENDGRID_USERNAME") => 'Albania Accel'),    
+        public $smtp = array(    
+        'transport' => 'Smtp',    
+        'from' => array('app20466457@heroku.com' => 'Albania Accel'),    
         'host' => 'smtp.sendgrid.net',    
         'port' => 587,    
-        'username' => 'app19805266@heroku.com',    
-        'password' => 'w8or1w97',
-		'timeout' => 30,
-		'client' => null,
-		'log' => true
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
+        'username' => 'app20466457@heroku.com',    
+        'password' => 'yokrz3fb',
+        'log' => true);
 	);    
 }
